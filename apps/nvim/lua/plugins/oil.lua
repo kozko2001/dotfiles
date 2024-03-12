b3503978f5e0294212ec3fbdf28c7213c93f12c6
Jordi Coscolla <kozko2001@gmail.com>
2024-03-12 13:52:33 +0100

    oil.nvim shows hidden files

--- apps/nvim/lua/plugins/oil.lua
+++ apps/nvim/lua/plugins/oil.lua
@@ -1,14 +1,22 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    { "-", function ()
-      require("oil").open()
-    end, desc = "open parent dir" },
-  },
-  config = function ()
-    require("oil").setup()
-  end
+	"stevearc/oil.nvim",
+	opts = {},
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"-",
+			function()
+				require("oil").open()
+			end,
+			desc = "open parent dir",
+		},
+	},
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
+	end,
 }
