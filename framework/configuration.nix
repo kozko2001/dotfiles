2371c44ad3dc410fd685c1fb7d55f01c721a0e1f
Jordi Coscolla <kozko2001@gmail.com>
2025-05-09 15:37:32 +0200

    add tailscale again

--- framework/configuration.nix
+++ framework/configuration.nix
@@ -225,4 +225,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
+  services.tailscale.enable = true;
 }
